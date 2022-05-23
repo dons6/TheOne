1bf89072ce2f24a2935f8644706662fbd3684c26
dons6 <jdonlan644@gmail.com>
2022-05-23 02:47:08 -0500

    amplify and routing

--- the-one/src/app/app-routing.module.ts
+++ the-one/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // { path: '/home', component: HomeComponent },
-  // { path: '/login', component: LoginComponent },
+  { path: '', component: AppComponent}
 ];
 
 @NgModule({
